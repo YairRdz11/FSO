5a2d5c79063c5c3d42a2647da10cf8182de197ac
Yair <francisco.rodriguez@ipicyt.edu.mx>
2018-07-11 11:50:01 -0500

    Foto Studio Olivo beta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { app_routing } from './app.routing';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/shared/menu/menu.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { IndexComponent } from './components/index/index.component';
+import { GaleryComponent } from './components/galery/galery.component';
+import { PackagesComponent } from './components/packages/packages.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { GoToTopComponent } from './components/go-to-top/go-to-top.component';
+import { PreloaderComponent } from './components/preloader/preloader.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    ContactComponent,
+    IndexComponent,
+    GaleryComponent,
+    PackagesComponent,
+    CarouselComponent,
+    GoToTopComponent,
+    PreloaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
